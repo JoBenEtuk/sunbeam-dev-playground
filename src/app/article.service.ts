8f9419d1227396723f79273ddd14a65b764f5ce6
JoBenEtuk <66846455+JoBenEtuk@users.noreply.github.com>
2020-10-20 17:58:02 +0100

    Update article.service.ts

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -59,21 +59,3 @@ export class ArticleService {
       }
     ]
   }
-  set(key: string, data: any): void {
-    try {
-      localStorage.setItem(key, JSON.stringify(data));
-    } catch (e) {
-      console.error('Error saving to localStorage', e);
-    }
-  }
-
-  get(key: string) {
-    try {
-      return JSON.parse(localStorage.getItem(key));
-    } catch (e) {
-      console.error('Error getting data from localStorage', e);
-      return null;
-    }
-  }
-
-}
